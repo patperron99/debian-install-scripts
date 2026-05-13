72853d463e3ab19704d8ce372f195853cfc7fb1c
sysadmin <sysadmin@server.localdomain>
2026-05-13 08:15:36 -0400

    Add screensaver to power menu, remove unused wlogout config

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -3,15 +3,16 @@
 # Installed to ~/.local/bin/powermenu.sh by setup-sway-config.sh
 # Bound to SUPER+SHIFT+P in sway config
 
-OPTIONS="Lock\nLogout\nSuspend\nHibernate\nReboot\nShutdown"
+OPTIONS="Screensaver\nLock\nLogout\nSuspend\nHibernate\nReboot\nShutdown"
 CHOICE=$(printf '%b' "$OPTIONS" | wofi --dmenu --prompt "Power" \
-    --width 200 --height 250 --no-actions --insensitive)
+    --width 200 --height 290 --no-actions --insensitive)
 
 case "$CHOICE" in
-    Lock)      hyprlock ;;
-    Logout)    swaymsg exit ;;
-    Suspend)   systemctl suspend ;;
-    Hibernate) systemctl hibernate ;;
-    Reboot)    systemctl reboot ;;
-    Shutdown)  systemctl poweroff ;;
+    Screensaver) bash ~/.local/bin/screensaver-launch.sh ;;
+    Lock)        hyprlock ;;
+    Logout)      swaymsg exit ;;
+    Suspend)     systemctl suspend ;;
+    Hibernate)   systemctl hibernate ;;
+    Reboot)      systemctl reboot ;;
+    Shutdown)    systemctl poweroff ;;
 esac
